56db78e7739efa81beb3aa2c90f2c8d934a39740
ITHYO <s2017i08@e-mirim.hs.kr>
2018-11-20 20:37:43 +0900

    이슈 10번

--- app/src/main/java/kr/hs/emirim/lg/mycatcode/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/lg/mycatcode/MainActivity.java
@@ -2,12 +2,26 @@ package kr.hs.emirim.lg.mycatcode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //System.out.println("OnCraeted가 실행됨.");
+        Log.v("MainActivity", "onCreated 가 실행");
+        Log.w("MainActivity", "Log.w 실행");
+        Log.i("MainActivity", "Log.i실행");
+        Log.d("MainActivity", "Log.d 실행");
+        Log.v("MainActivity", "Log.v 실행");
         setContentView(R.layout.activity_main);
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
 }
