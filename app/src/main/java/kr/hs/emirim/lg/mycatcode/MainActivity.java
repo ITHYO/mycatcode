b2c47b903000e99cdeb5e522123b5dbf7293f720
ITHYO <s2017i08@e-mirim.hs.kr>
2018-11-20 20:58:23 +0900

    11번 커밋

--- app/src/main/java/kr/hs/emirim/lg/mycatcode/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/lg/mycatcode/MainActivity.java
@@ -5,16 +5,22 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+final static String TAG="메인액티비티";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //System.out.println("OnCraeted가 실행됨.");
-        Log.v("MainActivity", "onCreated 가 실행");
-        Log.w("MainActivity", "Log.w 실행");
-        Log.i("MainActivity", "Log.i실행");
-        Log.d("MainActivity", "Log.d 실행");
-        Log.v("MainActivity", "Log.v 실행");
+//        Log.v("MainActivity", "onCreated 가 실행");
+//        Log.w("MainActivity", "Log.w 실행");
+//        Log.i("MainActivity", "Log.i실행");
+//        Log.d("MainActivity", "Log.d 실행");
+//        Log.v("MainActivity", "Log.v 실행");
+        Log.v(TAG, "onCreated 가 실행");
+        Log.w(TAG, "Log.w 실행");
+        Log.i(TAG, "Log.i실행");
+        Log.d(TAG, "Log.d 실행");
+        Log.v(TAG, "Log.v 실행");
         setContentView(R.layout.activity_main);
 
     }
--- app/src/main/java/kr/hs/emirim/lg/mycatcode/SplashActivity.java
+++ app/src/main/java/kr/hs/emirim/lg/mycatcode/SplashActivity.java
@@ -2,6 +2,7 @@ package kr.hs.emirim.lg.mycatcode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Log.d("스플래스액티비티", "onCretat 실행");
+
     }
 }
